bde1178bd8d015ccc7b0ee3a38485e540c865fda
Safeya-Yasien <bluesky7yas@gmail.com>
2024-08-19 20:35:46 +0300

    add test images

---
+++ src/assets/imgs/index.ts
@@ -0,0 +1,14 @@
+import hero from "./homeImgs/hero-2.svg";
+import courseOne from "./coursesImgs/course-1.jpg";
+
+export const HOMEIMGS = {
+  hero,
+};
+
+export const COURSESIMGS = {
+  courseOne,
+};
+
+const IMAGES = { HOMEIMGS, COURSESIMGS };
+
+export default IMAGES;
