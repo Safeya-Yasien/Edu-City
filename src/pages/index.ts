7e60793cde608dba35c65e26bb5d4a313919082b
Safeya-Yasien <bluesky7yas@gmail.com>
2024-08-19 20:45:13 +0300

    update

---
+++ src/pages/index.ts
@@ -0,0 +1,12 @@
+import Home from "./Home/Home";
+import Courses from "./Courses/Courses";
+import Contact from "./Contact/Contact";
+import Error from "./Error/Error";
+import SingleCourse from "./SingleCourse/SingleCourse";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+
+
+
+
+export { Home, Courses, Contact, Error, SingleCourse,Login ,Register};
