2c1fd4e368ac6eff94d7f3f807529a41d412b783
Safeya-Yasien <bluesky7yas@gmail.com>
2024-08-19 20:32:01 +0300

    add pages routing

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,51 @@
+import { MainLayout } from "@layouts/index";
+import {
+  Contact,
+  Courses,
+  Error,
+  Home,
+  Login,
+  Register,
+  SingleCourse,
+} from "@pages/index";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "courses",
+        element: <Courses />,
+      },
+      {
+        path: "courses/course-single",
+        element: <SingleCourse />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+const AppRouter = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default AppRouter;
