137d0eb2f55653f44ab89a43bdb92f31ad046a69
Safeya-Yasien <bluesky7yas@gmail.com>
2024-08-19 20:39:51 +0300

    main layout of pages

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,7 +1,25 @@
-import "./styles.module.scss";
+import { Header } from "@components/common";
+
+import { Outlet, useLocation } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import styles from "./styles.module.scss";
+
+const {  homeWrapper } = styles;
 
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+  return (
+    <>
+      <Header />
+      <div className={` py-5 ${isHomePage ? homeWrapper : ""}`}>
+        <Container>
+          <Outlet />
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default MainLayout;
